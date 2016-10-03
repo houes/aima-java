5eaffd9d33b5bc55140ea381517ecbde48c3c738
Guangyu <houes@iastate.edu>
2016-10-03 18:19:42 -0500

    initial setup

---
+++ aima-gui/src/main/java/cs572_lab2/ReversiGame.java
@@ -0,0 +1,62 @@
+package cs572_lab2;
+
+import java.util.List;
+
+import aima.core.search.adversarial.Game;
+import aima.core.util.datastructure.XYLocation;
+
+/**
+ * Provides an implementation of the Tic-tac-toe game which can be used for
+ * experiments with the Minimax algorithm.
+ * 
+ * @author Ruediger Lunde
+ * 
+ */
+public class ReversiGame implements Game<ReversiState, XYLocation, String> {
+
+	ReversiState initialState = new ReversiState();
+
+	@Override
+	public ReversiState getInitialState() {
+		return initialState;
+	}
+
+	@Override
+	public String[] getPlayers() {
+		return new String[] { ReversiState.X, ReversiState.O };
+	}
+
+	@Override
+	public String getPlayer(ReversiState state) {
+		return state.getPlayerToMove();
+	}
+
+	@Override
+	public List<XYLocation> getActions(ReversiState state) {
+		return state.getUnMarkedPositions();
+	}
+
+	@Override
+	public ReversiState getResult(ReversiState state, XYLocation action) {
+		ReversiState result = state.clone();
+		result.mark(action);
+		return result;
+	}
+
+	@Override
+	public boolean isTerminal(ReversiState state) {
+		return state.getUtility() != -1;
+	}
+
+	@Override
+	public double getUtility(ReversiState state, String player) {
+		double result = state.getUtility();
+		if (result != -1) {
+			if (player == ReversiState.O)
+				result = 1 - result;
+		} else {
+			throw new IllegalArgumentException("State is not terminal.");
+		}
+		return result;
+	}
+}
---
+++ aima-gui/src/main/java/cs572_lab2/Reversi_swing.java
@@ -0,0 +1,172 @@
+package cs572_lab2;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.BorderFactory;
+import javax.swing.Box;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JToolBar;
+
+import aima.core.search.adversarial.AdversarialSearch;
+import aima.core.search.adversarial.AlphaBetaSearch;
+import aima.core.search.adversarial.IterativeDeepeningAlphaBetaSearch;
+import aima.core.search.adversarial.MinimaxSearch;
+import aima.core.search.framework.Metrics;
+import aima.core.util.datastructure.XYLocation;
+
+/**
+ * Simple graphical Tic-tac-toe game application. It demonstrates the Minimax
+ * algorithm for move selection as well as alpha-beta pruning.
+ * 
+ * @author Guangyu Hou
+ */
+public class Reversi_swing {
+
+	/** Used for integration into the universal demo application. */
+	public JFrame constructApplicationFrame() {
+		JFrame frame = new JFrame();
+		JPanel panel = new ReversiPanel();
+		frame.add(panel, BorderLayout.CENTER);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		return frame;
+	}
+
+	/** Application starter. */
+	public static void main(String[] args) {
+		JFrame frame = new Reversi_swing().constructApplicationFrame();
+		frame.setSize(800, 800);
+		frame.setVisible(true);
+	}
+
+	/** Simple panel to control the game. */
+	private static class ReversiPanel extends JPanel implements
+			ActionListener {
+		private static final long serialVersionUID = 1L;
+		JComboBox<String> strategyCombo;
+		JButton clearButton;
+		JButton proposeButton;
+		JButton[] squares;
+		JLabel statusBar;
+
+		ReversiGame game;
+		ReversiState currState;
+		Metrics searchMetrics;
+
+		/** Standard constructor. */
+		ReversiPanel() {
+			this.setLayout(new BorderLayout());
+			JToolBar tbar = new JToolBar();
+			tbar.setFloatable(false);
+			strategyCombo = new JComboBox<String>(new String[] { "Minimax",
+					"Alpha-Beta", "Iterative Deepening Alpha-Beta",
+					"Iterative Deepening Alpha-Beta (log)" });
+			strategyCombo.setSelectedIndex(1);
+			tbar.add(strategyCombo);
+			tbar.add(Box.createHorizontalGlue());
+			clearButton = new JButton("Clear");
+			clearButton.addActionListener(this);
+			tbar.add(clearButton);
+			proposeButton = new JButton("Propose Move");
+			proposeButton.addActionListener(this);
+			tbar.add(proposeButton);
+
+			add(tbar, BorderLayout.NORTH);
+			JPanel spanel = new JPanel();
+			spanel.setLayout(new GridLayout(8, 8));
+			add(spanel, BorderLayout.CENTER);
+			squares = new JButton[64];
+			Font f = new java.awt.Font(Font.SANS_SERIF, Font.PLAIN, 32);
+			for (int i = 0; i < 64; i++) {
+				JButton square = new JButton("");
+				square.setFont(f);
+				square.setBackground(Color.WHITE);
+				square.addActionListener(this);
+				squares[i] = square;
+				spanel.add(square);
+			}
+			statusBar = new JLabel(" ");
+			statusBar.setBorder(BorderFactory.createEtchedBorder());
+			add(statusBar, BorderLayout.SOUTH);
+
+			game = new ReversiGame();
+			actionPerformed(null);
+		}
+
+		/** Handles all button events and updates the view. */
+		@Override
+		public void actionPerformed(ActionEvent ae) {
+			searchMetrics = null;
+			if (ae == null || ae.getSource() == clearButton)
+				currState = game.getInitialState();
+			else if (!game.isTerminal(currState)) {
+				if (ae.getSource() == proposeButton)
+					proposeMove();
+				else {
+					for (int i = 0; i < 9; i++)
+						if (ae.getSource() == squares[i])
+							currState = game.getResult(currState,
+									new XYLocation(i % 3, i / 3));
+				}
+			}
+			for (int i = 0; i < 9; i++) {
+				String val = currState.getValue(i % 3, i / 3);
+				if (val == ReversiState.EMPTY)
+					val = "";
+				squares[i].setText(val);
+			}
+			updateStatus();
+		}
+
+		/** Uses adversarial search for selecting the next action. */
+		private void proposeMove() {
+			AdversarialSearch<ReversiState, XYLocation> search;
+			XYLocation action;
+			switch (strategyCombo.getSelectedIndex()) {
+			case 0:
+				search = MinimaxSearch.createFor(game);
+				break;
+			case 1:
+				search = AlphaBetaSearch.createFor(game);
+				break;
+			case 2:
+				search = IterativeDeepeningAlphaBetaSearch.createFor(game, 0.0,
+						1.0, 1000);
+				break;
+			default:
+				search = IterativeDeepeningAlphaBetaSearch.createFor(game, 0.0,
+						1.0, 1000);
+				((IterativeDeepeningAlphaBetaSearch<?, ?, ?>) search)
+						.setLogEnabled(true);
+			}
+			action = search.makeDecision(currState);
+			searchMetrics = search.getMetrics();
+			currState = game.getResult(currState, action);
+		}
+
+		/** Updates the status bar. */
+		private void updateStatus() {
+			String statusText;
+			if (game.isTerminal(currState))
+				if (game.getUtility(currState, ReversiState.X) == 1)
+					statusText = "X has won :-)";
+				else if (game.getUtility(currState, ReversiState.O) == 1)
+					statusText = "O has won :-)";
+				else
+					statusText = "No winner...";
+			else
+				statusText = "Next move: " + game.getPlayer(currState);
+			if (searchMetrics != null)
+				statusText += "    " + searchMetrics;
+			statusBar.setText(statusText);
+		}
+	}
+}
